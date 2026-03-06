1903e5167d2a74b135e0a9452735560dd6efa7d0
JagadeepK2006 <jagadeep0210@gmail.com>
2026-03-06 16:53:10 +0530

    Create Java End-of-file.java

---
+++ 06.03.2026/Java End-of-file.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        String c = scanner.nextLine();
+        scanner.close();
+        System.out.println("1 "+a);
+        System.out.println("2 "+b);
+        System.out.println("3 "+c);    
+    }
+}
