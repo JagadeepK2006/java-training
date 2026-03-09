7396360a5f3660f31a9f45d599cd9e77ff15ac9f
JagadeepK2006 <jagadeep0210@gmail.com>
2026-03-09 17:41:37 +0530

    Create Java Int to String.java

---
+++ 09.03.2026/Java Int to String.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.security.*;
+import java.math.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String s = Integer.toString(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+        }
+    }
+}
